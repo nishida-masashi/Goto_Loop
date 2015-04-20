c984ba8286b57c1f148fc78d184a551e0ad2f095
m_nishida <sbi16tmg3ishropi7171@i.softbank.jp>
2015-04-21 08:35:49 +0900

    a

--- goto_loop/test/test.cpp
+++ goto_loop/test/test.cpp
@@ -7,17 +7,15 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	int i=0, j=0, k=0, l=0;
 
-	li: if(i <= 9)
-	lj: if(j <= 9)
-	lk: if(k <= 9)   
-	ll: if(l < 9) 
-	{             printf("%d%d%d%d   \n",i,j,k,++l); goto ll;}
+	top:
+	if(i <= 9) if(j <= 9) if(k <= 9) if(l < 9) 
+	{             printf("%d%d%d%d   \n",i,j,k,++l); goto top;}
 	else if (k != 9)
-	{l=0;         printf("%d%d%d%d \n\n",i,j,++k,l); goto lk;}
+	{l=0;         printf("%d%d%d%d \n\n",i,j,++k,l); goto top;}
 	else if (j != 9)
-	{l=0;k=0;     printf("%d%d%d%d   \n",i,++j,k,l); goto ll;}
+	{l=0;k=0;     printf("%d%d%d%d \n\n",i,++j,k,l); goto top;}
 	else if (i != 9)
-	{l=0;k=0;j=0; printf("%d%d%d%d   \n",++i,j,k,l); goto ll;}
+	{l=0;k=0;j=0; printf("%d%d%d%d \n\n",++i,j,k,l); goto top;}
 
 	return 0;
 }
